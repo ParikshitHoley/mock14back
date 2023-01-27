ffba6d6e3a4e30f573f7c6e75650012657f14315
ParikshitHoley <parikshitholey20@gmail.com>
2023-01-27 14:23:18 +0530

    added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,6 +10,18 @@ exports.addUser = async (req , res)=>{
     }
 }
 
+const { User } = require("../modals/user.modal")
+
+
+exports.addUser = async (req , res)=>{
+    try{
+        const user = await User.create(req.body);
+        return res.send(user)
+    }catch(e){
+        return res.status(500).send(e.message)
+    }
+}
+
 exports.dashboard = async (req , res)=>{
     try {
         const users = await User.find()
@@ -21,8 +33,16 @@ exports.dashboard = async (req , res)=>{
     }
 }
 
-exports.randomWord = async (req , res)=>{
-    try{
+exports.randomWord = async (req, res) => {
+    try {
+        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let randomWord = ""
+        let num = Math.floor(Math.random() * 5)
+        for (let i = 0; i < num; i++)
+        {
+        randomWord += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return res.send(randomWord)
      
     }catch(e){
         return res.status(500).send(e.message)
